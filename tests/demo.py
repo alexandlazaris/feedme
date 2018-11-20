efca16ee980122a36cae2e1550d2806454423e02
alexandlazaris <alex.lazaris617@gmail.com>
2018-11-20 12:35:07 +1100

    Removed old test file

--- tests/demo.py
+++
@@ -1,9 +0,0 @@
-from .. import player
-
-testPlayer = player.PlayerStart("Alexander", 10, 10, 10)
-
-def test_basic():
-    assert 1 == 1
-
-def test_playerHealthIsSet():
-    assert testPlayer.getHealthValue() != 0
