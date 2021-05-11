ed77a608459143be5eb2c87aa4b06e34e39a88f7
Alexander Lazaris <alexander.lazaris@weareroam.com>
2021-05-11 11:05:10 +1000

    output player stats to file & run process to generate cli graph

--- game/main.py
+++ game/main.py
@@ -2,11 +2,21 @@ import time
 import os
 import random
 from player import PlayerStart as newPlayerStart
+import subprocess
 
 class StartGame(object):
 	'A player object with default fields'
-	newPlayer = newPlayerStart("Alexander", 5, 1, 1)
+	newPlayer = newPlayerStart("Alexander", 1, 1, 1)
 	while True:
 		newPlayer.lvlup()
-		print (newPlayer.getAllStats())
+		file = open("./game/player.dat", "w")
+		file.truncate(0)
+		file.write("@ Health,Attack,Defence\n")
+		health = str(newPlayer.getHealthValue())
+		attack = str(newPlayer.getAttackValue())
+		defence = str(newPlayer.getDefenceValue())
+		file.write(f"Stats,{health},{attack},{defence}")
+		file.close()
+		subprocess.call("clear", shell=True)
+		subprocess.call("termgraph game/player.dat --color {blue,red,green} --title 'Let the games begin'", shell=True)
 		time.sleep(3)
